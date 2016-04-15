e69a5d8bf94df87c2fc58a7657ea59d9d63b3c3c
Brian Gerkey <gerkey@osrfoundation.org>
2016-04-15 12:32:16 -0700

    move typedef headers into roscpp and edit to use new-old name for diagnostic_msgs

--- roscpp/include/diagnostic_msgs/DiagnosticArray.h
+++ roscpp/include/diagnostic_msgs/DiagnosticArray.h
@@ -1,11 +1,11 @@
 #ifndef DIAGNOSTIC_MSGS__DIAGNOSTIC_ARRAY_H
 #define DIAGNOSTIC_MSGS__DIAGNOSTIC_ARRAY_H
 
-#include <diagnostics_msgs/msg/diagnostic_array.hpp>
+#include <diagnostic_msgs/msg/diagnostic_array.hpp>
 
 namespace diagnostic_msgs
 {
-  typedef ::diagnostics_msgs::msg::DiagnosticArray DiagnosticArray;
+  typedef ::diagnostic_msgs::msg::DiagnosticArray DiagnosticArray;
 }
 
 #endif
---
+++ roscpp/include/diagnostic_msgs/DiagnosticStatus.h
@@ -0,0 +1,13 @@
+#ifndef DIAGNOSTIC_MSGS__DIAGNOSTIC_STATUS_H
+#define DIAGNOSTIC_MSGS__DIAGNOSTIC_STATUS_H
+
+#include <diagnostic_msgs/msg/diagnostic_status.hpp>
+#include <diagnostic_msgs/msg/key_value.hpp>
+
+namespace diagnostic_msgs
+{
+  typedef ::diagnostic_msgs::msg::DiagnosticStatus DiagnosticStatus;
+  typedef ::diagnostic_msgs::msg::KeyValue KeyValue;
+}
+
+#endif
