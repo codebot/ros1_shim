cbae06fa61ed2e5f82e653423ff788535507bb90
Morgan Quigley <morgan@osrfoundation.org>
2016-05-03 17:58:57 -0700

    fix rosidl typesupport linkage

--- message_generation/cmake/generate_messages.cmake
+++ message_generation/cmake/generate_messages.cmake
@@ -1,5 +1,6 @@
 find_package(rosidl_cmake REQUIRED)
 find_package(rosidl_generator_cpp REQUIRED)
+find_package(rmw REQUIRED)
 macro(generate_messages)
   cmake_parse_arguments(ARG "" "" "DEPENDENCIES;LANGS" ${ARGN})
   message("HELLO I AM GENERATING YOUR MESSAGES: ${${PROJECT_NAME}_MESSAGE_FILES}")
@@ -14,6 +15,26 @@ macro(generate_messages)
       ${${PROJECT_NAME}_MESSAGE_FILES}
     )
   endif()
+  #message("     HERE ARE YOUR CATKIN_LIBRARIES: [ ${catkin_LIBRARIES} ]")
+  # maybe use get_default_rmw_implementation()   in rmw_implementation_cmake
+  get_default_rmw_implementation(rmw_impl)
+  message("default rmw: ${rmw_impl}")
+  get_rmw_typesupport(typesupport_impl "${rmw_impl}" LANGUAGE "cpp")
+  message("typesupport impl: ${typesupport_impl}")
+  set(_typesupport_target ${PROJECT_NAME}_gencpp__${typesupport_impl})
+  list(APPEND catkin_LIBRARIES ${_typesupport_target})
+  # TODO (codebot): break up rosidl_target_interfaces so that we don't 
+  # duplicate its functionality here. This is extremely fragile since any
+  # change in rosidl_target_interfaces will cause breakage here.
+  get_target_property(_include_directories ${_typesupport_target} INTERFACE_INCLUDE_DIRECTORIES)
+  # it would be better to append this to catkin_INCLUDE_DIRS but in many cases
+  # users will have already called include_directories(${catkin_INCLUDE_DIRS})
+  # before calling generate_messages()
+  include_directories(${_include_directories})
+  #list(APPEND catkin_INCLUDE
+  #rosidl_target_interfaces(${PROJECT_NAME}_gencpp    ${typesupport_impl} 
+  #get_rmw_typesupport(
+  #message("     NOW I HAVE APPENDED SOME STUFF: [ ${catkin_LIBRARIES} ]")
 endmacro()
 include_directories(
   ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_cpp/${PROJECT_NAME}/msg/shim
